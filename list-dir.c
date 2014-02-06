09eb6c0aceac8af7254a4fe2a502122bfbd9d28b
Jan Klemkow <j.klemkow@wemelug.de>
2014-02-06 18:20:48 +0100

    Add a tiny program to list direktories as html page

---
+++ list-dir.c
@@ -0,0 +1,47 @@
+#include <sys/types.h>
+
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "list-dir.h"
+
+void
+usage(void)
+{
+	fprintf(stderr, "list-dir [PATH]\n");
+}
+
+int
+main(int argc, char**argv)
+{
+	DIR *dir = NULL;
+	struct dirent *entry;
+	char *path = ".";
+
+	if (argc > 1)
+		path = strdup(argv[1]);
+
+	if ((dir = opendir(path)) == NULL)
+		goto err;
+
+	puts(DOCUMENT_HEADER);
+
+	while ((entry = readdir(dir)) != NULL) {
+		printf("\t<li>%s</li>\n", entry->d_name);
+	}
+
+	puts(DOCUMENT_FOOTER);
+
+	if (closedir(dir) != 0)
+		goto err;
+
+	return EXIT_SUCCESS;
+ err:
+	if (errno != 0)
+		printf("XXX: error\n");
+
+	return EXIT_FAILURE;
+}
---
+++ list-dir.h
@@ -0,0 +1,18 @@
+#ifndef LIST_DIR_H
+#define LIST_DIR_H
+
+#define DOCUMENT_HEADER 	\
+	"<!DOCTYPE html>\n"	\
+	"<html>\n"		\
+	"<head>\n"		\
+	"\t<title>dir</title>\n"\
+	"</head>\n"		\
+	"<body>\n"		\
+	"<ul>"
+
+#define DOCUMENT_FOOTER	\
+	"</ul>\n"	\
+	"</body>\n"	\
+	"</html>"
+
+#endif
